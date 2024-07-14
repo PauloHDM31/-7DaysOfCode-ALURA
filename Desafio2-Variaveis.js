88690176180ef24ad7aa42b1a749df762c03e9ca
Paulo <PAULOhdm236@gmail.com>
2024-07-14 17:48:10 -0300

    2° DESAFIO

---
+++ Desafio2-Variaveis.js
@@ -0,0 +1,5 @@
+let nome = window.prompt("Qual é o seu nome?");
+let idade = window.prompt("Qual a sua idade?");
+let linguagem = window.prompt("Qual linguagem de programação você está estudando?");
+
+window. alert(`Olá ${nome}, você tem ${idade} e já está aprendendo ${linguagem}!`)
