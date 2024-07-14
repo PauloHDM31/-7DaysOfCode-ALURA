3c462f7d2317bfc491b4451ad898836901888cdd
Paulo <PAULOhdm236@gmail.com>
2024-07-14 18:05:15 -0300

    2° Desafio

--- Desafio2-Variaveis.js
+++ Desafio2-Variaveis.js
@@ -3,3 +3,13 @@ let idade = window.prompt("Qual a sua idade?");
 let linguagem = window.prompt("Qual linguagem de programação você está estudando?");
 
 window. alert(`Olá ${nome}, você tem ${idade} e já está aprendendo ${linguagem}!`)
+let res = window.prompt(`Você gosta de estudar ${linguagem} Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+If (res == 1);{
+    window.alert("Muito bom! Continue estudando e você terá muito sucesso.")
+} 
+If (res == 2);{
+    window.alert("Ahh que pena... Já tentou aprender outras linguagens?")
+} 
+    
+
